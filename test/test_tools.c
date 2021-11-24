cb54ac9e6c29d1e604d676eff21dd796abee58e6
tin-gprieto <martin.gonzalez.prieto@gmail.com>
2021-11-24 02:00:48 -0300

    fix(test_tools): se adecuan test a las nuevas modificaciones de tools

--- test/test_tools.c
+++ test/test_tools.c
@@ -6,20 +6,20 @@
 int main(){
 
     const char* extension = "csv";
-    char str_vacio [MAX_STR]= "";
-    char str_sin_extension [MAX_STR]= "hola";
-    char str_con_dos_extensiones_incorrecto [MAX_STR]= "hola.csv.hola";
-    char str_con_dos_extensiones_correcto [MAX_STR]= "hola.hola.csv";
-    char str_correcto [MAX_STR]= "hola.csv";
-    char str_incorrecto [MAX_STR]= "hola.txt";
+    const char str_vacio [MAX_STR]= "";
+    const char str_sin_extension [MAX_STR]= "hola";
+    const char str_con_dos_extensiones_incorrecto [MAX_STR]= "hola.csv.hola";
+    const char str_con_dos_extensiones_correcto [MAX_STR]= "hola.hola.csv";
+    const char str_correcto [MAX_STR]= "hola.csv";
+    const char str_incorrecto [MAX_STR]= "hola.txt";
     
     test_nuevo_grupo("Test corroborar extension");
-    test_afirmar(!ruta_corroborar_extension(str_vacio, extension), "Un string vacío no corrobora");
-    test_afirmar(!ruta_corroborar_extension(str_sin_extension, extension), "Un string sin extension no corrobora"); 
-    test_afirmar(ruta_corroborar_extension(str_correcto, extension), "Un string que tiene la extensión corrobora");
-    test_afirmar(!ruta_corroborar_extension(str_incorrecto, extension), "Un string que tiene la extensión distinta no corrobora");
-    test_afirmar(ruta_corroborar_extension(str_con_dos_extensiones_correcto, extension),  "Un string con otro punto pero termina con la extensión corrobora");
-    test_afirmar(!ruta_corroborar_extension(str_con_dos_extensiones_incorrecto, extension), "Un string con otro punto y sin que termine con la extensión no corrobora");
+    test_afirmar(!ruta_cumple_extension(str_vacio, extension), "Un string vacío no corrobora");
+    test_afirmar(!ruta_cumple_extension(str_sin_extension, extension), "Un string sin extension no corrobora"); 
+    test_afirmar(ruta_cumple_extension(str_correcto, extension), "Un string que tiene la extensión corrobora");
+    test_afirmar(!ruta_cumple_extension(str_incorrecto, extension), "Un string que tiene la extensión distinta no corrobora");
+    test_afirmar(ruta_cumple_extension(str_con_dos_extensiones_correcto, extension),  "Un string con otro punto pero termina con la extensión corrobora");
+    test_afirmar(!ruta_cumple_extension(str_con_dos_extensiones_incorrecto, extension), "Un string con otro punto y sin que termine con la extensión no corrobora");
     test_mostrar_reporte();
     test_nuevo_grupo("Test barra de porcentaje");
     print_barra_porcentaje(0,100,10);
